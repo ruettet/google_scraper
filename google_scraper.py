bf6926035043511c18d68462a766b2f8bf76b977
Tom Ruette <tom.ruette@sirris.be>
2016-05-25 20:40:06 +0200

    made the thing a bit more verbose in the output plus ngram support

--- google_scraper.py
+++ google_scraper.py
@@ -2,11 +2,15 @@ from bs4 import BeautifulSoup
 from requests import get
 from re import sub
 from sys import argv
+from nltk.util import ngrams
+from time import sleep
 
 
 class GoogleScraper():
-  def __init__(self, search_ngram):
-    first_page_url = "http://www.google.com/search?q=" + '"' + search_ngram.replace(" ", "+") + '"'
+  def __init__(self, search_ngram, sleep_length=10, next_word="Volgende"):
+    first_page_url = "https://www.google.nl/search?q=" + '"' + search_ngram.replace(" ", "+") + '"'
+    self.sleep_length = float(sleep_length)
+    self.next_word = next_word
     self.list_of_urls = []
     self.get_urls(first_page_url)
 
@@ -18,20 +22,35 @@ class GoogleScraper():
       self.list_of_urls.extend(urls)
 
   def get_info_from_page(self, url):
-    soup = BeautifulSoup(get(url).text, "lxml")
+    sleep(self.sleep_length)
+    soup = BeautifulSoup(get(url, headers={'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}).text, "lxml")
     return self.get_next_page_url(soup), self.get_search_hit_urls(soup)
 
   def get_next_page_url(self, soup):
     nav = soup.find("table", id="nav")
-    next_page = nav(text="Volgende")
-    return "http://www.google.com" + next_page[0].parent.parent["href"] if len(next_page) > 0 else None
+    if nav:
+      next_page = nav(text=self.next_word)
+      return "https://www.google.nl" + next_page[0].parent.parent["href"] if len(next_page) > 0 else None
+    else:
+      return None
 
   def get_search_hit_urls(self, soup):
     return [sub("&sa.+", "", hit.a["href"].lstrip("/url?q=")) for hit in soup.findAll("h3", {"class": "r"})]
 
 
 if __name__ == "__main__":
-  '$ python google_scraper.py "geen unieke barcode"'
-  gs = GoogleScraper(argv[1])
-  print("\n".join(gs.list_of_urls))
-  
+  '$ python google_scraper.py test.txt 10 Volgende test_out.csv'
+  urls = {}
+  with open(argv[1], "r") as f:
+    for ngram in ngrams(f.read().split(), 5):
+      search_term = " ".join(ngram)
+      print(search_term)
+      gs = GoogleScraper(search_term, argv[2], argv[3])
+      urls[search_term] = gs.list_of_urls
+  out = []
+  for ngram in urls:
+    print(ngram, urls[ngram])
+    for url in urls[ngram]:
+      out.append(",".join([ngram, url.lstrip("http://").split("/")[0], url]))
+  with open(argv[4], "w") as f:
+    f.write("\n".join(out))
